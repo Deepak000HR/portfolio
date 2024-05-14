55c4e83f3e15446fb1404281990268edc83e2616
mg3994 <manishgautammg7@yahoo.com>
2024-05-14 23:07:04 +0530

    headernav

--- components/Helper/ServiceCard.tsx
+++ components/Helper/ServiceCard.tsx
@@ -1,26 +1,33 @@
 import React from "react";
-import { CommandLineIcon,
+import {
+  CommandLineIcon,
   ServerIcon,
   CodeBracketIcon,
   CursorArrowRaysIcon,
   RocketLaunchIcon,
-  BugAntIcon
-   } from "@heroicons/react/16/solid";
+  BugAntIcon,
+} from "@heroicons/react/16/solid";
 interface Props {
   title: string;
   num: string;
 }
 
-const iconMapping ={
+const iconMapping = {
   "01": CommandLineIcon,
   "02": ServerIcon,
   "03": CodeBracketIcon,
   "04": CursorArrowRaysIcon,
   "05": RocketLaunchIcon,
-  "06": BugAntIcon
-}
+  "06": BugAntIcon,
+};
 const ServiceCard = ({ title, num }: Props) => {
-  return <div>ServiceCard</div>;
+  const IconComponent =
+    (iconMapping as Record<string, React.ElementType>)[num] || CommandLineIcon;
+  return (
+    <div className="bg-black custom__service z-[100] relative transform rounded-2x1 text-center p-6 shadow-md">
+      ServiceCard
+    </div>
+  );
 };
 
 export default ServiceCard;
