80752ba3519d00f0a4fb1c6295767e902b784710
mg3994 <manishgautammg7@yahoo.com>
2024-05-15 21:48:44 +0530

    headernav

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -15,9 +15,10 @@ const Skills = ({ id }: Props) => {
             <span className="text-yellow-300">Skills</span> & Experience
           </h1>
           <p className="text-[15px] mb-[1.5rem] text-white opacity-70 mt-[1.5rem]">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
-            repellendus amet exercitationem inventore quae, debitis ea tenetur
-            labore quam tempora.
+            Over the years, I've honed a diverse set of skills across various
+            fields. From technical proficiencies in software tools to soft
+            skills like effective communication and time management, each skill
+            has played a pivotal role in my professional growth and success.
           </p>
           <button className="relative flex h-[50px] w-40 items-center justify-center font-semibold overflow-hidden bg-red-500 text-white shadow-2xl transition-all before:absolute before:h-0 before:w-0 before:rounded-full before:bg-blue-600 before:duration-500 before:ease-out hover:before:h-56 hover:before:w-56">
             <span className="relative z-10">Learn More</span>
