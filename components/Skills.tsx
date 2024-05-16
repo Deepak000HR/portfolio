0206c086e827024c21168060eee8ed49a12dd891
mg3994 <manishgautammg7@yahoo.com>
2024-05-16 09:34:17 +0530

    headernav

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -12,7 +12,7 @@ const Skills = ({ id }: Props) => {
           <p className="heading__mini">My Skills</p>
           <h1 className="heading__primary">
             Let's Explore Popular
-            <span className="text-yellow-300">Skills</span>& Experience
+            <span className="text-yellow-300"> Skills</span> & Experience
           </h1>
           <p className="text-[15px] mb-[1.5rem] text-white opacity-70 mt-[1.5rem]">
             Over the years, I've honed a diverse set of skills across various
@@ -20,7 +20,10 @@ const Skills = ({ id }: Props) => {
             skills like effective communication and time management, each skill
             has played a pivotal role in my professional growth and success.
           </p>
-          <button className="relative flex h-[50px] w-40 items-center justify-center font-semibold overflow-hidden bg-red-500 text-white shadow-2xl transition-all before:absolute before:h-0 before:w-0 before:rounded-full before:bg-blue-600 before:duration-500 before:ease-out hover:before:h-56 hover:before:w-56">
+          <button
+            onClick={() => (window.location.href = "./resume.pdf")}
+            className="relative flex h-[50px] w-40 items-center justify-center font-semibold overflow-hidden bg-red-500 text-white shadow-2xl transition-all before:absolute before:h-0 before:w-0 before:rounded-full before:bg-blue-600 before:duration-500 before:ease-out hover:before:h-56 hover:before:w-56"
+          >
             <span className="relative z-10">Learn More</span>
           </button>
         </div>
